8918a16f2223a2711d609cdbd85805ba6d4ba0a6
A K <gugie194@gmail.com>
2020-06-06 12:09:47 +0300

    Added nameof.

--- templates/ReactRedux/ReactRedux/ClientApp/src/app/App.tsx
+++ templates/ReactRedux/ReactRedux/ClientApp/src/app/App.tsx
@@ -6,11 +6,8 @@ import Layout from './Layout/Layout';
 import Home from '../components/Home';
 import Counter from '../components/Counter';
 import FetchData from '../components/FetchData';
-import { RuntimeConfig } from '../RuntimeConfig';
 
 export const App = () => {
-    console.log(RuntimeConfig.AuthorityUrl);
-    console.log(RuntimeConfig.MyServiceUrl);
     return (
         <Layout>
             <Route exact path='/' component={Home} />
---
+++ templates/ReactRedux/ReactRedux/ClientApp/src/utils/nameof.ts
@@ -0,0 +1,20 @@
+export type valueOf<T> = T[keyof T];
+export function nameOf<T, V extends T[keyof T]>(f: (x: T) => V): valueOf<{ [K in keyof T]: T[K] extends V ? K : never }>;
+export function nameOf(f: (x: any) => any): keyof any {
+    var p = new Proxy({}, {
+        get: (target, key) => key
+    })
+    return f(p);
+}
+
+export interface I_$<T> {
+    nameOf<V extends T[keyof T]>(f: (x: T) => V): valueOf<{ [K in keyof T]: T[K] extends V ? K : never }>;
+}
+
+export function _$<T>(obj: T) {
+    return {
+        nameOf: (f: (x: any) => any) => {
+            return nameOf(f);
+        }
+    } as I_$<T>;
+}
