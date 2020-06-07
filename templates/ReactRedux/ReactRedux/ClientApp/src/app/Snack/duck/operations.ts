68b64490dc860f6a84221630c4d9526ad7b7e6de
A K <gugie194@gmail.com>
2020-06-07 12:04:26 +0300

    fix

--- templates/ReactRedux/ReactRedux/ClientApp/src/app/Snack/duck/operations.ts
+++ templates/ReactRedux/ReactRedux/ClientApp/src/app/Snack/duck/operations.ts
@@ -1,13 +1,10 @@
 import { AppThunkAction } from "../../../store";
 import { SnackActionTypes, openSnack, closeSnack, SnackType } from ".";
 
+let closeTimeout = 0;
 export const openTimedSnack = (type: SnackType, message: string | undefined, onUndo?: () => void): AppThunkAction<SnackActionTypes> => (dispatch) => {
     dispatch(openSnack(type, message, onUndo));
-    dispatch(startCloseTimer());
-}
 
-let closeTimeout = 0;
-export const startCloseTimer = (): AppThunkAction<SnackActionTypes> => (dispatch) => {
     window.clearTimeout(closeTimeout);
     closeTimeout = window.setTimeout(() => {
         dispatch(closeSnack());
