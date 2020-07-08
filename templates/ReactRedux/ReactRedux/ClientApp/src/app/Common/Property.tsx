98111e11c5f525618db150af874905d6a7fab6c9
A K <gugie194@gmail.com>
2020-07-08 23:03:04 +0300

    Added children for Property.

--- templates/ReactRedux/ReactRedux/ClientApp/src/app/Common/Property.tsx
+++ templates/ReactRedux/ReactRedux/ClientApp/src/app/Common/Property.tsx
@@ -4,73 +4,64 @@ import clsx from 'clsx';
 import { useHistory } from 'react-router-dom';
 import { formatMoney, formatMMYY, formatDate, formatDateTime, formatTimespan } from '../../utils/formatHelper';
 
+export type PropertyVariant = 'mmyydate' | 'date' | 'datetime' | 'timespan' | 'bool' | 'money' | undefined;
+
 export interface PropertyProps {
     label: string;
     value: any;
-    variant?: 'mmyydate' | 'date' | 'datetime' | 'timespan' | 'bool' | 'money';
+    className?: string;
+    variant?: PropertyVariant;
     gridXs?: boolean | "auto" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | undefined;
     link?: string;
     displayIf?: boolean;
     onClick?: () => void;
 }
 
-export const Property: React.FC<PropertyProps> = ({ label, value, variant, gridXs, link, displayIf, onClick }) => {
-    const classes = useStyles();
-    const history = useHistory();
+export const Property: React.FC<PropertyProps> =
+    ({ label, value, className, variant, gridXs, link, displayIf, onClick, children }) => {
+        const classes = useStyles();
+        const history = useHistory();
 
-    if (displayIf === false) {
-        return null;
+        if (displayIf === false) {
+            return <></>;
+        }
+
+        value = formatValueByVariant(value, variant);
+
+        const divClassName = clsx('m-2', className);
+
+        const element = (
+            <div
+                className={onClick || link ? clsx(divClassName, classes.clickableDiv) : divClassName}
+                onClick={() => {
+                    const selectedText = getSelection()?.toString();
+                    if (!selectedText) {
+                        onClick?.();
+                        link && history.push(link);
+                    }
+                }}
+            >
+                <Typography variant="caption" className='text-muted' gutterBottom>
+                    {label}
+                </Typography>
+                <Typography variant="subtitle2" className={classes.value} gutterBottom>
+                    {value}
+                </Typography>
+            </div>
+        );
+
+        return (
+            <Grid
+                item={gridXs !== undefined}
+                xs={gridXs}
+                container={children ? true : undefined}
+                direction={children ? 'row' : undefined}
+            >
+                {element}
+                {children}
+            </Grid>
+        );
     }
-
-    switch (variant) {
-        case 'mmyydate':
-            value = value ? formatMMYY(value) : 'Нет';
-            break;
-        case 'date':
-            value = value ? formatDate(value) : 'Нет';
-            break;
-        case 'datetime':
-            value = value ? formatDateTime(value) : 'Нет';
-            break;
-        case 'timespan':
-            value = value ? formatTimespan(value) : 'Нет';
-            break;
-        case 'bool':
-            value = value ? 'Да' : 'Нет'
-            break;
-        case 'money':
-            value = formatMoney(value);
-            break;
-    }
-
-    const divClass = (onClick || link)
-        ? clsx('m-2', classes.clickableDiv)
-        : 'm-2';
-
-    const element = (
-        <div
-            className={divClass}
-            onClick={() => {
-                const selectedText = getSelection()?.toString();
-                if (!selectedText) {
-                    onClick?.();
-                    link && history.push(link);
-                }
-            }}
-        >
-            <Typography variant="caption" className='text-muted' gutterBottom>
-                {label}
-            </Typography>
-            <Typography variant="subtitle2" className={classes.value} gutterBottom>
-                {value}
-            </Typography>
-        </div>
-    );
-
-    return gridXs !== undefined
-        ? <Grid item xs={gridXs}>{element}</Grid>
-        : element;
-}
 
 const useStyles = makeStyles({
     value: {
@@ -84,3 +75,22 @@ const useStyles = makeStyles({
         },
     }
 });
+
+const formatValueByVariant = (value: string, variant: PropertyVariant): string => {
+    switch (variant) {
+        case 'mmyydate':
+            return value ? formatMMYY(value) : 'Нет';
+        case 'date':
+            return value ? formatDate(value) : 'Нет';
+        case 'datetime':
+            return value ? formatDateTime(value) : 'Нет';
+        case 'timespan':
+            return value ? formatTimespan(value) : 'Нет';
+        case 'bool':
+            return value ? 'Да' : 'Нет'
+        case 'money':
+            return formatMoney(value);
+        default:
+            return value;
+    }
+}
